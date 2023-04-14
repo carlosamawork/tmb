3319c61ed5a73021aa485443ecc668a40e976528
Cacho Salvador <carlos@ama.work>
2023-04-14 14:51:51 +0200

    Primera search

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,11 +1,45 @@
-import SearchComponent from 'components/SearchComponent';
 import fs from 'fs';
+import { drupal } from 'lib/drupal';
+import { useState } from 'react';
 
 export default function Search() {
+    const [query, setQuery] = useState('')
+    const [results, setResults] = useState([])
+
+    const handleInputChange = event => {
+        setQuery(event.target.value)
+    }
+
+    const handleFormSubmit = async event => {
+        event.preventDefault()
+
+        const index = await drupal.getSearchIndex('search')
+
+        console.log(index)
+
+       
+    }
     return (
         <>
             <main>
-                <SearchComponent />
+                <div className='searchContent'>
+                    <form onSubmit={handleFormSubmit}>
+                        <label htmlFor="search">Buscar:</label>
+                        <input type="text" id="search" value={query} onChange={handleInputChange} />
+                        <button type="submit">Buscar</button>
+                    </form>
+
+                    {results.length > 0 && (
+                        <ul>
+                        {results.map(result => (
+                            <li key={result.id}>
+                            <a href={result.url}>{result.title}</a>
+                            <p>{result.summary}</p>
+                            </li>
+                        ))}
+                        </ul>
+                    )}
+                </div>
             </main>
         </>
     );
