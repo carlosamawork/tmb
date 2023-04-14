e9bf12179312c0105d5ae091fa521a064f1a007d
Cacho Salvador <carlos@ama.work>
2023-04-14 13:20:28 +0200

    Primera search

---
+++ pages/search.tsx
@@ -0,0 +1,12 @@
+import SearchComponent from 'components/SearchComponent';
+import fs from 'fs';
+
+export default function Search() {
+    return (
+        <>
+            <main>
+                <SearchComponent />
+            </main>
+        </>
+    );
+}
